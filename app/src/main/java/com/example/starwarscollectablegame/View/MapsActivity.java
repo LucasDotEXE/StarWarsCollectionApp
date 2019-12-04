94bd57730f2cb43c00d9006da1dbe4c3712d6956
Marisakis <sebcolijn@live.nl>
2019-12-04 13:13:03 +0100

    Basic Notification setup
    Includes default Android code for Notificationchannel. Update for proper use later

--- app/src/main/java/com/example/starwarscollectablegame/View/MapsActivity.java
+++ app/src/main/java/com/example/starwarscollectablegame/View/MapsActivity.java
@@ -1,9 +1,17 @@
 package com.example.starwarscollectablegame.View;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
+import com.example.starwarscollectablegame.NotificationActivity;
 import com.example.starwarscollectablegame.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,7 +20,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+ {
 
     private GoogleMap mMap;
 
@@ -20,11 +29,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        //Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        createNotificationChannel();
+
+
+
     }
+
+    private void createNotificationChannel()
+    {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            new NotificationChannel(getString(R.string.channel_id), name, importance);
+            NotificationChannel channel = new NotificationChannel(getString(R.string.channel_id), name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    /*Method that generates a default notification that starts a Notificationactivity*/
+     //todo: rework for use in displaying details of the topic that has been updated
+    private void sendDefaultNotification()
+    {
+        Intent intent = new Intent(this, NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, getString(R.string.channel_id))
+                .setSmallIcon(R.drawable.ic_message_black_24dp)
+                .setContentTitle(getString(R.string.update_model_title))
+                .setContentText(getString(R.string.update_model_text1) + " " + "Millenium Falcon")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(1, builder.build());
+    }
+
 
 
     /**
