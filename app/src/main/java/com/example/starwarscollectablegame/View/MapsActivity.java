612ff088c569e696dc1e1a8493185ae81f93f90e
Lucas Prins <lljPrins@avans.nl>
2019-12-06 14:36:03 +0100

    Added Theme

--- app/src/main/java/com/example/starwarscollectablegame/View/MapsActivity.java
+++ app/src/main/java/com/example/starwarscollectablegame/View/MapsActivity.java
@@ -2,6 +2,7 @@ package com.example.starwarscollectablegame.View;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -17,18 +18,24 @@ import com.example.starwarscollectablegame.Model.StarwarsData.Starship;
 import com.example.starwarscollectablegame.Model.StarwarsData.SwapiEntry;
 import com.example.starwarscollectablegame.Model.StarwarsData.Vehicle;
 import com.example.starwarscollectablegame.R;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, SwapiEntryListener, SwapiEntryPageListener {
 
     private GoogleMap mMap;
+
+    private StarwarsApiManager apiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +44,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        StarwarsApiManager apiManager = new StarwarsApiManager(this);
+        this.apiManager = new StarwarsApiManager(this);
 
 //        apiManager.getSwapiEntry(this, 1, StarWarsDataType.FILM);
 //        apiManager.getSwapiEntry(this, 1, StarWarsDataType.PEOPLE);
@@ -46,7 +53,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //        apiManager.getSwapiEntry(this, 7, StarWarsDataType.STARSHIP);
 //        apiManager.getSwapiEntry(this, 7, StarWarsDataType.VIHICLE);
 
-        apiManager.getSwapiEntryPage(this, 1, StarWarsDataType.FILM);
+        apiManager.getSwapiEntryPage(this, 1, StarWarsDataType.SPECIES);
 
         mapFragment.getMapAsync(this);
     }
@@ -63,42 +70,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        mMap = googleMap;;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng avans = new LatLng(51.5773462, 4.7962926);
+        mMap.addMarker(new MarkerOptions().position(avans).title("Marker in Avans"));
+        mMap.setMinZoomPreference(15);
+        mMap.setMaxZoomPreference(20);
+        mMap.setBuildingsEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setCompassEnabled(true);
+//        mMap.setMapType();
+
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(CameraPosition.builder().target(avans).tilt(45).build()));
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.sith_style_json)); //raw vind hem wel gewoon
+//                            this, R.raw.jedi_style_json)); //raw vind hem wel gewoon
+
+            if (!success) {
+                Log.e("MAPACtivity", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("MAPACtivity", "Can't find style. Error: ", e);
+        }
+
     }
 
     @Override
     public void onEntryAvailavle(SwapiEntry object, StarWarsDataType type) {
-        switch (type) {
-            case FILM: {
-                Log.e("TEST", ((Film) object).toString());
-                break;
-            }
-            case PEOPLE: {
-                Log.e("TEST", ((People) object).toString());
-                break;
-            }
-            case PLANET: {
-                Log.e("TEST", ((Planet) object).toString());
-                break;
-            }
-            case SPECIES: {
-                Log.e("TEST", ((Species) object).toString());
-                break;
-            }
-            case VIHICLE: {
-                Log.e("TEST", ((Vehicle) object).toString());
-                break;
-            }
-            case STARSHIP: {
-                Log.e("TEST", ((Starship) object).toString());
-                break;
-            }
-        }
+//        switch (type) {
+//            case FILM: {
+//                Log.e("TEST", ((Film) object).toString());
+//                break;
+//            }
+//            case PEOPLE: {
+//                Log.e("TEST", ((People) object).toString());
+//                break;
+//            }
+//            case PLANET: {
+//                Log.e("TEST", ((Planet) object).toString());
+//                break;
+//            }
+//            case SPECIES: {
+//                Log.e("TEST", ((Species) object).toString());
+//                break;
+//            }
+//            case VIHICLE: {
+//                Log.e("TEST", ((Vehicle) object).toString());
+//                break;
+//            }
+//            case STARSHIP: {
+//                Log.e("TEST", ((Starship) object).toString());
+//                break;
+//            }
+//        }
     }
 
     @Override
@@ -109,33 +140,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onSwapiEntryPageListener(ArrayList<SwapiEntry> entries, StarWarsDataType type, int nextPage) {
-        switch (type) {
-            case FILM: {
-                for (SwapiEntry entry : entries) {
-                    Log.e("TEST", ((Film) entry).toString());
-                }
-                break;
-            }
-            case PEOPLE: {
-//                Log.e("TEST", ((People) object).toString());
-                break;
-            }
-            case PLANET: {
-//                Log.e("TEST", ((Planet) object).toString());
-                break;
-            }
-            case SPECIES: {
-//                Log.e("TEST", ((Species) object).toString());
-                break;
-            }
-            case VIHICLE: {
-//                Log.e("TEST", ((Vehicle) object).toString());
-                break;
-            }
-            case STARSHIP: {
-//                Log.e("TEST", ((Starship) object).toString());
-                break;
-            }
+//        switch (type) {
+//            case FILM: {
+//                for (SwapiEntry entry : entries) {
+//                    Log.e("TEST", ((Film) entry).toString());
+//                }
+//                break;
+//            }
+//            case PEOPLE: {
+////                Log.e("TEST", ((People) object).toString());
+//                break;
+//            }
+//            case PLANET: {
+////                Log.e("TEST", ((Planet) object).toString());
+//                break;
+//            }
+//            case SPECIES: {
+//                for (SwapiEntry entry : entries) {
+//                    Log.e("TEST", ((Species) entry).toString());
+//                }
+//                break;
+//            }
+//            case VIHICLE: {
+////                Log.e("TEST", ((Vehicle) object).toString());
+//                break;
+//            }
+//            case STARSHIP: {
+////                Log.e("TEST", ((Starship) object).toString());
+//                break;
+//            }
+//        }
+        if (nextPage != -1) {
+            apiManager.getSwapiEntryPage(this, nextPage, type);
         }
     }
 
